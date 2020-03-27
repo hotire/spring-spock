17e86c5facb4b5686a6feb21e497520b1f3c8e07
hotire <gngh0101@gmail.com>
2020-03-27 23:25:19 +0900

    init

---
+++ src/main/java/com/github/hotire/spring/spock/SpringSpockApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.spock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hotire/spring/spock/SpringSpockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.spock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
