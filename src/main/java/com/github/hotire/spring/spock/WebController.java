cd12dea0f16bcc456ff676b4ade5cf9d1a5fc534
hotire <gngh0101@gmail.com>
2020-03-30 00:45:57 +0900

    Add WebController to handle hello

--- src/main/java/com/github/hotire/spring/spock/WebController.java
+++ src/main/java/com/github/hotire/spring/spock/WebController.java
@@ -1,7 +1,16 @@
 package com.github.hotire.spring.spock;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/hello")
 public class WebController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("Hello world!");
+    }
 }
