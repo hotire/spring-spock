84373ced5925188332cd2334e5bed9ab91f1f229
hotire <gngh0101@gmail.com>
2020-03-30 00:46:41 +0900

    Verify status 200 and 'Hello world! when hello

---
+++ src/test/groovy/com/github/hotire/spring/spock/WebControllerTest.groovy
@@ -0,0 +1,26 @@
+package com.github.hotire.spring.spock
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import spock.lang.Specification
+
+@AutoConfigureMockMvc
+@WebMvcTest(WebController)
+class WebControllerTest extends Specification {
+
+    @Autowired
+    private MockMvc mvc
+
+    def "when get is performed then the response has status 200 and content is 'Hello world!'"() {
+        expect: "Status is 200 and the response is 'Hello world!'"
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .response
+                .contentAsString == "Hello world!"
+    }
+}
