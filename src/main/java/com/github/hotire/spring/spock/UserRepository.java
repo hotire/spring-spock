43b4309e2d9bf25829753bc10f842a8f7fbbfcbb
hotire <gngh0101@gmail.com>
2020-04-26 22:59:20 +0900

    Add Repository annoation

--- src/main/java/com/github/hotire/spring/spock/UserRepository.java
+++ src/main/java/com/github/hotire/spring/spock/UserRepository.java
@@ -1,7 +1,10 @@
 package com.github.hotire.spring.spock;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository {
     Optional<User> findById(Long id);
 }
